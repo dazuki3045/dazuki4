0bd2716048741722cd2ec691d8bbbc270dac94b0
dazuki3045 <nikolay.tekh@gmail.com>
2025-02-11 13:48:32 +0600

    fix background color for card
    fix background color for card

--- three/src/components/card/card.jsx
+++ three/src/components/card/card.jsx
@@ -2,7 +2,7 @@ import styles from "./card.module.css"
 
 export const Card = ({title, date, complete }) => {
     return (
-        <div className={`${styles.card} ${complete && styles.grey}`}>
+        <div className={`${styles.card} ${complete && styles.white}`}>
             <h2>{title}</h2>
             <p>{date}</p>
             {complete && <button className={styles.btn}>Done</button>}
